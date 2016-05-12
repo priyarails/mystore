7750e2ba35b0e8c9c02153e9e828b3147e83d37a
@priyarails <priya.shunmuga@gmail.com>
2016-05-12 14:37:00 +0530

    add to cart+thumbnail

---
+++ app/assets/javascripts/spree/frontend.js
@@ -0,0 +1,8 @@
+//= require bootstrap-sprockets
+//= require jquery.validate/jquery.validate.min
+//= require spree
+//= require spree/jquery.elevatezoom
+//= require spree/jquery-1.8.3
+//= require spree/frontend/cart
+//= require spree/frontend/checkout
+//= require spree/frontend/product
